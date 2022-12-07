0f388df24e155c07ef0f03e217bd4f702a223911
randaDoni <randa.doni303@gmail.com>
2022-12-07 16:17:59 +0700

    tes

--- resources/views/dashboardUser.blade.php
+++ resources/views/dashboardUser.blade.php
@@ -17,15 +17,15 @@
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="dashboard/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="dashboard/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="dashboard/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="dashboard/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="dashboard/aos/aos.css" rel="stylesheet">
+  <link href="{{URL::to('/dashboard/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{URL::to('/dashboard/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{URL::to('/dashboard/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  <link href="{{URL::to('/dashboard/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{URL::to('/dashboard/aos/aos.css')}}" rel="stylesheet">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="{{URL::to('/assets/css/main.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: PhotoFolio - v1.1.1
@@ -35,7 +35,7 @@
   ======================================================== -->
 
 
-<body>
+
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="hero d-flex flex-column justify-content-center align-items-center" data-aos="fade" data-aos-delay="1500">
     <div class="container">
@@ -113,15 +113,14 @@
   </div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{URL::to('dashboard/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{URL::to('dashboard/swiper/swiper-bundle.min.js"></script')}}"></script>
+  <script src="{{URL::to('dashboard/glightbox/js/glightbox.min.js"></script')}}"></script>
+  <script src="{{URL::to('dashboard/aos/aos.js"></script>')}}"></script>
+  <script src="{{URL::to('dashboard/php-email-form/validate.js"></script')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{URL::to('/assets/js/main.js')}}"></script>
 
 </body>
 
-</html>
