170b57c4df526ebf5b7301b31e6d050d2ce61412
Savira Almas Nur Arifin <saviraarifin02.sana@gmail.com>
2022-12-15 14:38:30 +0700

    ganti general, atur tampilan decline, navbar tulisan hitam, dll

--- resources/views/admin/logout.blade.php
+++ resources/views/admin/logout.blade.php
@@ -8,7 +8,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>Log Out</title>
+      <title>Dashboard Admin</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
@@ -43,13 +43,13 @@
                <div class="sidebar_blog_1">
                   <div class="sidebar-header">
                      <div class="logo_section">
-                        <a href="index.html"><img class="logo_icon img-responsive" src="assets/img/logos/tubes.png" alt="#" /></a>
+                        <a href="/"><img class="logo_icon img-responsive" src="assets/img/logos/tubes.png" alt="#" /></a>
                      </div>
                   </div>
                   <div class="sidebar_user_info">
                      <div class="icon_setting"></div>
                      <div class="user_profle_side">
-                        <div class="user_img"><img class="img-responsive" src="assets/img/logos/tubes.png" alt="#" /></div>
+                        <a href="/"><div class="user_img"><img class="img-responsive" src="assets/img/logos/tubes.png" alt="#" /></div></a>
                         <div class="user_info">
                            <h6>NSC</h6>
                            <p><span class="online_animation"></span> Online</p>
@@ -58,7 +58,7 @@
                   </div>
                </div>
                <div class="sidebar_blog_2 text-light">
-                  <a href="/dsbadmin"><h4>General</h4></a>
+                  <a href="/dashboardadmin"><h4>Dashboard Admin</h4></a>
                   <ul class="list-unstyled components">
                      <li class="active"><a href="/data_user"><i class="fa fa-briefcase blue1_color"></i> <span>Data User</span></a></li>
                      <li class="active"><a href="/post"><i class="fa fa-map purple_color2"></i> <span>Post</span></a></li>
@@ -73,17 +73,12 @@
                <div class="topbar">
                   <nav class="navbar navbar-expand-lg navbar-light">
                      <div class="full">
-                        <button type="button" id="sidebarCollapse" class="sidebar_toggle"><i class="fa fa-bars"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <button type="button" id="sidebarCollapse" class="sidebar_toggle"><i class="fa fa-bars"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="27" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                          </svg></i></button>
 
                         <div class="right_topbar">
                            <div class="icon_info">
-                              <ul>
-                                 <li><a href="#"><i class="fa fa-bell-o"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-                                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
-                                  </svg></i><span class="badge">2</span></a></li>
-                              </ul>
                               <ul class="user_profile_dd">
                                  <li>
                                     <img class="img-responsive rounded-circle" style="width: 35px; height: 35px;" src="assets/img/logos/tubes.png" alt="#" /><span class="name_user">NSC</span>
@@ -123,7 +118,7 @@
                      </div>
                      <div class="row column4 graph">
                         <div class="col-md-12">
-                          //isi nanti
+                        
                         </div>
                      </div>
                   </div>
